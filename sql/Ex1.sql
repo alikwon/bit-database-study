94d994b4032616c4889ea5b0c94855af92bc982b
alikwon <63032589+alikwon@users.noreply.github.com>
2020-05-23 14:10:44 +0900

    Update Ex1.sql

--- sql/Ex1.sql
+++ sql/Ex1.sql
@@ -457,14 +457,18 @@ where custid=1;
 select count(*) from orders 
 where custid = 1; -- count 안에 컬럼명을 써도되지만 *가 더 안정적일듯
 
+
+
+
+
 --(5) 박지성이구매한도서의출판사수
 select count(distinct(publisher))"출판사의 수"
 from book
 where bookid in (select bookid
                 from orders
                 where custid = (select custid
-                                from customer
-                                where name = '박지성'));
+                                        from customer
+                                        where name = '박지성'));
 
 
 --(6) 박지성이구매한도서의이름, 가격, 정가와판매가격의차이
@@ -523,21 +527,24 @@ from orders;
 --(10) 고객의이름과고객별구매액
 select c.name, e.price
 from customer c inner join(select custid,sum(saleprice) as price
-                           from orders
-                           group by custid) e
+                                     from orders
+                                     group by custid) e
 on c.custid = e.custid
 order by c.custid;
 
-select c.name, sum(o.saleprice)
+select c.name, sum(o.saleprice) "price"
 from customer c, orders o
-where c.custid=o.custid
-group by c.custid;
-
+where c.custid = o.custid
+group by c.name
+;
 --(11) 고객의이름과고객이구매한도서목록
 select name, bookname
 from customer c cross join orders o cross join book b
-where c.custid = o.custid and o.bookid = b.bookid
-order by c.custid;
+where c.custid = o.custid 
+   and o.bookid = b.bookid
+order by c.custid
+;
+
 
 --(12) 도서의가격(Book 테이블)과판매가격(Orders 테이블)의차이가가장많은주문
 --(13) 도서의판매액평균보다자신의구매액평균이더높은고객의이름
