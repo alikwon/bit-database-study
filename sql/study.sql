111f9631673e40a1125cc960c1ebd9b50b00d666
alikwon <63032589+alikwon@users.noreply.github.com>
2020-05-24 22:00:12 +0900

    Update study.sql

--- sql/study.sql
+++ sql/study.sql
@@ -10,3 +10,27 @@ select distinct deptno, job
 from emp
 order by deptno;
 
+-- sal 정렬 후 같은 sal있을때 이름 정렬
+select *
+from emp
+order by sal,ename;
+
+select initcap('qeWERsfdbEFJjhGjh')
+from dual;
+
+
+select ename, deptno, rpad(substr(ename,0,2), length(ename), '*')
+from emp;
+
+select nvl2(comm, sal+comm, sal) as comm
+from emp;
+
+select avg(sal), '10' as deptno from emp where deptno=10;
+
+select deptno,job,avg(sal), count(*)
+from emp
+where sal <2000
+group by deptno, job
+having avg(sal) < 3000
+order by deptno;
+
