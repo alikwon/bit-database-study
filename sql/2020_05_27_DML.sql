30c7eb2e4454f7e6472509d465a672306ed25992
alikwon <63032589+alikwon@users.noreply.github.com>
2020-05-27 18:04:44 +0900

    Update 2020_05_27_DML.sql

--- sql/2020_05_27_DML.sql
+++ sql/2020_05_27_DML.sql
@@ -55,3 +55,139 @@ from emp
 
 select * from emp_hir02;
 select * from emp_sal;
+
+
+----------------------------------------
+drop table emp01;
+
+create table emp01
+as
+select * from emp
+;
+
+--컬럼의 데이터 변경(수정)
+--update 테이블이름 set 컬럼이름1=값, 컬럼이름2=값, ...where 행을 찾는 조건식
+-- where 절이 없는경우 모든행이 영향을 받는다
+select * from emp01;
+--1. 모든사원의 부서번호를 30번으로 수정하자.
+update emp01
+set deptno=30
+;
+
+--2. 모든사원의 급여를 10%인상시키자
+update emp01
+set sal = sal*1.1;
+
+--3. 모든 사원의 입사일을 오늘로 수정시키자
+update emp01
+set hiredate = sysdate;
+
+select * from emp01;
+
+commit;
+
+rollback; -- 마지막 커밋 시점으로 간다.
+-----------------------------------------
+--1. 부서번호가 10번인 사원의 deptno를 30번으로 수정
+update emp01
+set deptno = 30
+where deptno =10;
+
+-- 2. 급여가 3000이상인 사원만 10%인상
+update emp01
+set sal = sal *1.1
+where sal >= 3000;
+
+--3. 1987년에 입사한 사원의 입사일이 오늘로 수정합시다. 
+--사원의 입사일을 오늘로 수정한 후에 테이블 내용을 살펴봅시다.
+update emp01
+set hiredate = sysdate
+where substr(hiredate,1,2)='87'
+;
+
+-----------------------------
+-- 복수 컬럼 값 변경
+------------------------------
+--1.SCOTT 사원의 부서번호는 10번으로, 
+-- 직급은 MANAGER로 한꺼번에 수정하도록 합시다.
+update emp01 
+set deptno=10, job= 'MANAGER'
+where ename = 'SCOTT';
+
+--2.SCOTT 사원의 입사일자는 오늘로, 
+-- 급여를 50 으로 커미션을 4000 으로 수정합시다.
+update emp01
+set sal= 50, comm=4000, hiredate = sysdate
+where ename = 'SCOTT';
+
+select * from emp01 where ename= 'SCOTT';
+
+------------------------------------------
+--서브쿼리로 데이터 수정
+
+--1. 20번 부서의 지역명을 40번 부서의 지역명으로 변경하기 위해서 
+-- 서브 쿼리문을 사용해 봅시다.
+drop table dept01;
+create table dept01
+as
+select * from dept;
+
+update dept01
+set loc = (
+	select loc
+	from dept01
+	where deptno=40
+	)
+	where deptno=20;
+select * from dept01;
+
+--서브 쿼리를 이용해서 부서번호가 20인 부서의 부서명과 지역명을
+-- 부서 번호가 40번인 부서와 동일하게 변경하도록 해 봅시다.
+update dept01
+set 
+dname = (
+	select dname
+	from dept01 
+	where deptno=40),
+loc = (
+	select loc
+	from dept01
+	where deptno=40)
+where deptno=20
+;
+
+update dept01
+set (dname, loc) = (
+	select dname, loc 
+	from dept
+	where deptno=40)
+where deptno=20
+;
+
+----------------------------------------
+-- DELETE 문
+
+--delete from 테이블명 where 조건
+--where 절이 없으면 모든 테이블에 영향
+
+-- 부서테이블의 모든행을 삭제
+delete from dept01;
+select * from dept01;
+-- 물리적으로 삭제되는것이 아닌 논리적으로 삭제된 상태
+-- commit 조건에 맞추는 것이 아닌이상 다른프로그램에서는 여전히 데이터 확인가능
+
+--부서 테이블에서 30번 부서만 삭제
+rollback;
+delete from dept01
+where deptno=30;
+-- 삭제하는 특성은 최대한PK 기반으로 삭제하는것이 좋다
+-- 은연중에 중복되는 데이터를 의도와는 다르게 같이 삭제할수 있기때문
+
+--사원 테이블에서 부서명이 SALES인 사원을 모두 삭제
+delete from emp01
+where deptno = (
+	select deptno 
+	from dept01 
+	where dname ='SALES')
+;
+select * from emp01;
