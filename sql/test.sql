a72dc41c0f2dbf2c938fd6fa75e530220ae391f0
alikwon <63032589+alikwon@users.noreply.github.com>
2020-06-02 09:59:36 +0900

    Update test.sql

--- sql/test.sql
+++ sql/test.sql
@@ -2,7 +2,7 @@ alter table emp01
 modify hiredate default sysdate;
 
 insert into emp01 (empno, ename, job,mgr,hiredate,sal,comm,deptno) 
-values (to_number('1111'),'SON','MANAGER',to_number(''),to_date('sysdate','YY/MM/DD'),3000,null,20);
+values (to_number('1111'),'SON','MANAGER',to_number(''),to_date('19881115','YY/MM/DD'),3000,null,20);
 delete from emp01 where empno = 1111;
 select * from emp01;
 rollback;
