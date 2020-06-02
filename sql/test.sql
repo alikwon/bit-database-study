b7f3fecf87ab9a31a3235034aeb2094de4f08007
alikwon <63032589+alikwon@users.noreply.github.com>
2020-06-02 14:55:39 +0900

    Update test.sql

--- sql/test.sql
+++ sql/test.sql
@@ -7,6 +7,39 @@ delete from emp01 where empno = 1111;
 select * from emp01;
 rollback;
 commit;
+--2.
+insert into dept(deptno,dname,loc) values(50,'DESIGN','SEOUL');
+--3.
+update dept
+set dname = 'GAME', loc = 'JEJU'
+where deptno = 50;
+--4.
+delete from dept
+where deptno = 50;
+--5.
+select * from tab;
+--6.
+desc emp;
+--7.
+select * from user_constraints;
 
-select * from emp01 natural join dept;
+
+-- 1.
+create index emp_index on emp (ename);
+--2.
+create view emp_view
+as
+select *
+from emp natural join dept;
+--3.
+update emp01
+set deptno = (
+	select deptno
+	from emp01
+	where ename = 'SCOTT')
+;
+
+rollback;
+select * from emp_view;
+select * from emp01;
 delete from dept where deptno = 70;
